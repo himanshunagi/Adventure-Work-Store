128cc429beefaa42fccfbaf0cff62a93b9fbc0af
Himanshu Nagi <130278351+himanshunagi@users.noreply.github.com>
2024-04-16 12:51:03 +0530

    Add files via upload

---
+++ Dim_Calender.sql
@@ -0,0 +1,16 @@
+-- Cleansed DIM_Date Table --
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date,  
+  [EnglishDayNameOfWeek] AS Day, 
+  [EnglishMonthName] AS Month, 
+  Left([EnglishMonthName], 3) AS MonthShort,   -- Useful for front end date navigation and front end graphs.
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year --[CalendarSemester], 
+
+FROM 
+ [AdventureWorksDW2022].[dbo].[DimDate]
+
+WHERE 
+  CalendarYear >= 2023
